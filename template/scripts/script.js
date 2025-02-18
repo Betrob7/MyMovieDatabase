99a5506d76b2b7c4817dd75cd9ad1ce52df1751d
Robin Lindqvist <robinlindqvist@live.se>
2025-02-18 13:01:51 +0100

    fixed OMDB-API and added the search option

--- template/scripts/script.js
+++ template/scripts/script.js
@@ -1,21 +1,26 @@
 import { renderRandomTrailers } from "./modules/caroussel.js";
 import { renderTopMovies } from "./modules/topMovies.js";
+import { renderSearchedMovies } from "./modules/search.js";
+import { searchListener } from "./modules/eventHandlers.js";
 
 
-if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
+if(window.location.pathname === '/' || window.location.pathname.includes('index.html')) {
     console.log('index.html');
+    renderRandomTrailers();
+    renderTopMovies();
+    searchListener();
 
-} else if(window.location.pathname === '/favorites.html') {
+} else if(window.location.pathname.includes('favorites.html')) {
     console.log('favorites.html');
 
-} else if(window.location.pathname === '/movie.html') {
+} else if(window.location.pathname.includes('movie.html')) {
     console.log('movie.html');
 
-} else if(window.location.pathname === '/search.html') {
+} else if(window.location.pathname.includes('search.html')) {
     console.log('search.html');
+    renderSearchedMovies();
 
 }
 
-renderRandomTrailers();
-renderTopMovies();
+
 
